9329730dc5f158038e0cdf7b4603118fba01ea82
Lia Skalkos <hello@liaskalkos.net>
2015-06-04 15:18:50 -0400

    Working

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
 
+  if array.length > 2
+    last = array.pop
+    array.join(", ") + ", and #{last}"
+  elsif array.length > 1
+    last = array.pop
+    array.join(", ") + " and #{last}"
+  else
+    array.join
+  end
 end
